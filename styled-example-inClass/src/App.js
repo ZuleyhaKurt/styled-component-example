81e693ea772d1057e04c74450e7721d12b7efbbb
Züleyha Kurt <netice2017@gmail.com>
2022-10-25 23:10:36 +0300

    lesson end

--- styled-example-inClass/src/App.js
+++ styled-example-inClass/src/App.js
@@ -13,6 +13,7 @@ const App = () => {
   return (
     <>
       <ThemeProvider theme={style}>
+        {/* <GlobalStyles/> */}
         <Header />
         </ThemeProvider>
     </>
--- styled-example-inClass/src/components/Header.jsx
+++ styled-example-inClass/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Button from "../styles/Button.styled"
+import Flex from "../styles/Flex.styled"
 import StyledHeader, { Image, Logo, Nav } from "../styles/Header.styled"
 
 
@@ -12,14 +13,14 @@ const Header = () => {
             <Button bg="#a62240">Talk to Adviser</Button>
               </div>
           </Nav>
-          <div>
+          <Flex>
               <div>
               <h1>The It Career of Your Dreams Starts Here!</h1>
                 <p>Clarusway is a leading international software Bootcamp. Join a micro class online with other trainees and learn coding skills with a highly-skilled instructor.</p>
-              <Button>Start Your New Carier</Button>
+              <Button bg="#a62240" >Start Your New Carier</Button>
               </div>
-              <Image src="./images/courses.jpg"></Image>
-          </div>
+              <Image src="./images/hero.jpg"></Image>
+              </Flex>
 
       </StyledHeader>
       
